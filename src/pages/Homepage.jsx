677ec30f0322e8eab990ee880d71e5bc1fca75b4
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2022-11-05 15:22:50 +0100

    Only contact page left

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,15 +3,16 @@ import Footer from '../components/Footer/Footer'
 import Navbar from '../components/Navbar/Navbar'
 import About from '../components/About/About'
 import Landing from '../components/Landing/Landing'
+import './Homepage.css'
 
 const Homepage = () => {
   return (
-    <>
+    <div className='Homepage'>
     <Navbar/>
     <Landing/>
     <About/>
     <Footer/>
-    </>
+    </div>
   )
 }
 
